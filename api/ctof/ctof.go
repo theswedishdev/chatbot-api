578d386d24f246c6745e03d5f5cf08a793ed87c1
Joel Ericsson (bmc811) <joel.ericsson@teliacompany.com>
2021-07-25 00:52:03 +0200

    Formatting

--- api/ctof/ctof.go
+++ api/ctof/ctof.go
@@ -8,21 +8,21 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	
+
 	temperature := r.URL.Query().Get("temperature")
-	
-	if (temperature == "") {
+
+	if temperature == "" {
 		fmt.Fprintf(w, "Missing user input")
 		return
 	}
-	
+
 	temperatureInCelsius, err := strconv.ParseFloat(temperature, 64)
 	if err != nil {
 		fmt.Fprintf(w, "Invalid input")
 		return
 	}
-	
+
 	temperatureInFahrenheit := (temperatureInCelsius * 1.8) + 32
-	
+
 	fmt.Fprintf(w, "%.2f°F", temperatureInFahrenheit)
 }
--- api/ftoc/ftoc.go
+++ api/ftoc/ftoc.go
@@ -8,21 +8,21 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	
+
 	temperature := r.URL.Query().Get("temperature")
-	
-	if (temperature == "") {
+
+	if temperature == "" {
 		fmt.Fprintf(w, "Missing user input")
 		return
 	}
-	
+
 	temperatureInFahrenheit, err := strconv.ParseFloat(temperature, 64)
 	if err != nil {
 		fmt.Fprintf(w, "Invalid input")
 		return
 	}
-	
+
 	temperatureInCelsius := (temperatureInFahrenheit - 32) * 5 / 9
-	
+
 	fmt.Fprintf(w, "%.2f°C", temperatureInCelsius)
 }
